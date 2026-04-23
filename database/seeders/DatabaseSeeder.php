3bbcb0dc235b85d1611514deb659be0a9e4519e6
rizheez <rezamuhammad980@gmail.com>
2026-04-23 20:44:11 +0800

    refactor(database): comment out LegacyPostSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ComprehensiveDemoSeeder::class);
-        $this->call(LegacyPostSeeder::class);
+        // $this->call(LegacyPostSeeder::class);
     }
 }
